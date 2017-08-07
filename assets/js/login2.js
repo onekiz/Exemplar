1308cefd340715cdd8f53e7c49b0f5d8c7e41a35
Danny <dannynhois@gmail.com>
2017-08-07 00:36:40 -0500

    fixed registration on login page

--- assets/js/login2.js
+++ assets/js/login2.js
@@ -14,6 +14,8 @@ $('#register-form-link').click(function(e) {
   e.preventDefault();
 });
 
+//variable to set displayName
+var registeringNewUser = false;
 
 // Initialize Firebase
 var config = {
@@ -29,6 +31,9 @@ firebase.initializeApp(config);
 // ////////Listening Login with email and password////////
 $('#login-submit').on('click', function(e) {
   e.preventDefault();
+
+  //make sure authentication doesn't try to update display name
+  registeringNewUser = false;
   var auth = firebase.auth();
   var email = $('#username').val();
   var pass = $('#password').val();
@@ -37,19 +42,76 @@ $('#login-submit').on('click', function(e) {
   //if invalid login then display error message
   promise.catch(function(event) {
     $('#error-message').html(event.message);
-    console.log(event.message + ' deneme login');
   });
 
 
 });
 
+// ///////Submitting form to sign up clicking button.... all user info is stored in firebase with specific UID/////////
+$(document).on('click', '#register-submit', registerNewUser);
+
+function registerNewUser() {
+  registerNewUser = true;
+  event.preventDefault();
+  var auth = firebase.auth();
+  var users = {
+    email: $('#email').val(),
+    pass: $('#register-password').val()
+  };
+
+  // ////////Calling firebase method to create user account with email an password///////////
+  auth.createUserWithEmailAndPassword(users.email, users.pass).catch(function(error) {
+    // Handle Errors
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    console.log(users);
+    $('#register-error-message').html(errorMessage);
+  }).then(function(response) {
+    console.log('response: ', response)
+    console.log('name: ', $('#name').val());
+
+    // ///saving user info on firebase////////////////
+    // var storageRef = firebase.storage().ref(response.uid + '/' + file.name);
+    // storageRef.put(file);
+
+    var database = firebase.database();
+    console.log('got here');
+    firebase.database().ref('users/' + response.uid).set({
+      email: users.email,
+      pass: users.pass,
+      displayName: $('#name').val(),
+      lastName: $('#lastName').val(),
+      // paperTitle: '',
+      // paperTime: '',
+      // imgUrl: file.name
+    });
+
+  });
+}
+
 
 // //////event listenner whenever there is a change of Auth State signing in or signing up////////
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    console.log(user);
-    firstLogIn = false;
-    window.location = 'index.html';
+    if(registerNewUser){
+      //update displayname
+      user.updateProfile(
+        {
+          displayName: $('#name').val(),
+          // photoURL: file.name
+        }).then(function() {
+        console.log('Update is successfull');
+        window.location = 'index.html';
+      }, function(error) {
+        console.log('not able to update user info');
+        console.log(error);
+      });
+    } else{
+
+      window.location = 'index.html';
+    }
   } else {
     console.log('not logged in');
   }
